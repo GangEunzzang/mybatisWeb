7dfc7bd975dfcb47fffb19b571ac99b3c2d92d16
이강은 <rkddms123456@gmail.com>
2022-12-25 12:58:18 +0900

    mybatis delete

--- src/test/java/com/example/mybatisweb/MybatisWebApplicationTests.java
+++ src/test/java/com/example/mybatisweb/MybatisWebApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.mybatisweb;
 
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,22 +10,10 @@ import java.sql.Connection;
 @SpringBootTest
 class MybatisWebApplicationTests {
 
-    @Autowired
-    private SqlSessionFactory sqlSessionFactory;
 
     @Test
     void contextLoads() {
     }
 
-    @Test
-    public void connection_test () throws Exception {
-
-        try(Connection con = sqlSessionFactory.openSession().getConnection()){
-            System.out.println("커넥션 성공");
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-     }
 
 }
