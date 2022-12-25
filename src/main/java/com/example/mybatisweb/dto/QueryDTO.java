2bce456b467020dc2dd011dc9751ead209937a78
이강은 <rkddms123456@gmail.com>
2022-12-25 12:57:47 +0900

    validation

--- src/main/java/com/example/mybatisweb/dto/QueryDTO.java
+++ src/main/java/com/example/mybatisweb/dto/QueryDTO.java
@@ -2,10 +2,16 @@ package com.example.mybatisweb.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.pl.REGON;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class QueryDTO {
 
-    private Object query;
+    @NotBlank
+    private String query;
 }
